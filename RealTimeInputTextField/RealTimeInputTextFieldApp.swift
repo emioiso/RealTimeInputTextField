022701e30fc75e6e01f81dccd2892154aad54158
emioiso <jijjjiijiijijiji@gmail.com>
2023-10-17 12:06:55 +0900

    Initial Commit

---
+++ RealTimeInputTextField/RealTimeInputTextFieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealTimeInputTextFieldApp.swift
+//  RealTimeInputTextField
+//
+//  Created by emi oiso on 2023/10/17.
+//
+
+import SwiftUI
+
+@main
+struct RealTimeInputTextFieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
