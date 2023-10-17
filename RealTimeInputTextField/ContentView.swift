bb88f56dad6f3c15d13dcd7323451c1e20ca612c
emioiso <jijjjiijiijijiji@gmail.com>
2023-10-17 18:14:26 +0900

    .gitignoreと、TextFieldに入力された値がリアルタイムで更新される機能と、クリアボタンの実装。日本語入力するとグレーのTextFieldの文字が消えないのがわからない。

--- RealTimeInputTextField/ContentView.swift
+++ RealTimeInputTextField/ContentView.swift
@@ -1,24 +1,99 @@
+////
+////  ContentView.swift
+////  RealTimeInputTextField
+////
+////  Created by emi oiso on 2023/10/17.
+////
 //
-//  ContentView.swift
-//  RealTimeInputTextField
+//import SwiftUI
 //
-//  Created by emi oiso on 2023/10/17.
+//struct ContentView: View {
+//    @State var inputText = ""
 //
+//    var body: some View {
+//        VStack {
+//            Text("あなたの名前は？:　\(inputText)")
+//                .padding()
+//
+//            InputView(inputText: $inputText)
+//        }
+//    }
+//}
+//
+//struct InputView: View {
+//    @Binding var inputText: String
+//
+//    var body: some View {
+//        TextField("",text: $inputText)
+//            .modifier(TextFieldClearButton(text: $inputText))
+//            .background(.gray)
+//            .padding()
+//    }
+//}
+//
+//#Preview {
+//    ContentView()
+//}
 
 import SwiftUI
 
 struct ContentView: View {
+    @State var text: String = ""
+    @State var inputText = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("あなたの名前は？:　\(inputText)")
+                .padding()
+            
+            InputView(inputText: $inputText)
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct TextFieldClearButton: ViewModifier {
+    @Binding var text: String
+    
+    func body(content: Content) -> some View {
+        ZStack(alignment: .trailing)
+        {
+            content
+            if !text.isEmpty {
+                Button(
+                    action: {
+                        self.text = ""
+                    })
+                {
+                    Image(systemName: "xmark.circle.fill")
+//                    Image(systemName: "xmark.circle")
+                                         .foregroundColor(Color(UIColor.opaqueSeparator))
+                }
+                .padding(.trailing, 8)
+            }
+        }
+    }
+}
+
+
+struct InputView: View {
+    @Binding var inputText: String
+    
+    var body: some View {
+        TextField("",text: $inputText)
+            .modifier(TextFieldClearButton(text: $inputText))
+            .background(Color.gray) // 背景色をColorで設定
+        
+        
+//            .background(.gray)
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContentView()
+            ContentView()
+        }
+    }
 }
